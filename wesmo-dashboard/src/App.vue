b28316e25a72ec0129bd7410a6c4e72b0f1be5c6
0n3Appl3 <jedd.lupoy@gmail.com>
2023-04-28 10:50:22 +1200

    Experimenting with Vue components
    Added a complications template component.

--- wesmo-dashboard/src/App.vue
+++ wesmo-dashboard/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import ComplicationTemplate from './components/complications/ComplicationTemplate.vue';
 </script>
 
 <template>
@@ -14,6 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+      <ComplicationTemplate :size="1" />
     </div>
   </header>
 
---
+++ wesmo-dashboard/src/components/complications/ComplicationTemplate.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+export default {
+    props: {
+        size: Number,
+    },
+    data() {
+        return {
+            container: 'container',
+        }
+    },
+};
+</script>
+
+<template>
+    <div :class="container">
+        <p>Insert complication information<br/>Size: {{ size }}</p>
+    </div>
+</template>
+
+<style scoped>
+.container {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding: 1rem;
+	width: 15rem;
+	height: 15rem;
+	border-radius: 2rem;
+	background: var(--colour-background);
+	box-shadow: 10px 10px 30px var(--white-box-shadow-1),
+				-10px -10px 30px var(--white-box-shadow-2);
+}
+
+.container:hover {
+	box-shadow: inset 10px 10px 30px var(--white-box-shadow-1),
+				inset -10px -10px 30px var(--white-box-shadow-2);
+	cursor: pointer;
+}
+</style>
