ec2f0b97d4eb81c8ac9176c8ce02f5c103efd1b7
0n3Appl3 <jedd.lupoy@gmail.com>
2023-09-20 10:48:42 +1200

    Implemented LSTM neural network
    To be used instead of an MLP Regressor. Not yet tested.

--- digital-twin.py
+++ digital-twin.py
@@ -1,9 +1,12 @@
 from dotenv.main import load_dotenv
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
 from sklearn.neural_network import MLPRegressor
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler, normalize
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, normalize
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -18,7 +21,11 @@ sio = socketio.AsyncClient()
 # Error message prefix
 error_prefix = "[ERROR] "
 
+# MLP
 grid_search = None
+# LSTM
+normaliser = None
+model = Sequential()
 
 # Model features
 voltage = None
@@ -41,8 +48,11 @@ async def event(message: str):
         temperature = int(message_array[8], 16)
 
     if (voltage != None and current != None and temperature != None):
-        print('test')
+        print('we have a voltage, current and temperature value. using them to predict soc')
         # predict_soc()
+        voltage = None
+        current = None
+        temperature = None
     # Pretend results are processed for digital twin and send to server and then to all connected web browser clients
     # sio.emit("send-twin-results", random.randrange(1, 100, 2))
     # main()
@@ -91,9 +101,47 @@ def test_sql():
 #     print(post_response_json)
 
 #
-# Trains the MLP Regressor neural network.
+# Trains the LSTM neural network. (NOT YET TESTED)
 #
 def train_model():
+    global normaliser
+    # Read JSON data and convert into data frame.
+    json = '<insert data>'
+    df = pd.DataFrame(json)
+
+    # Specify the target column.
+    target_column = ['<insert target column>']
+
+    # Get the input and target values from columns.
+    X = df['<insert parameter column>'].values
+    y = df[target_column].values.ravel()
+
+    # Normalise the imput data
+    normaliser = MinMaxScaler()
+    X_normalised = normaliser.fit_transform(X)
+
+    # Split the data into train and test batches.
+    X_train, X_test, y_train, y_test = train_test_split(X_normalised, y, test_size=0.2, random_state=1)
+
+    # Make the LSTM neural network
+    lstm = LSTM(5, input_shape=(X_train.shape[1], X_train.shape[2]))
+    dense = Dense(1)
+    model.add(lstm)
+    model.add(dense)
+    model.compile(loss="mean_squared_error", optimiser="adam")
+
+    # Fit the model.
+    model.fit(X_train, y_train, epochs=100, batch_size=50, verbose=1)
+
+    # Evaluate model on test data
+    loss, accuracy = model.evaluate(X_test, y_test)
+    print("Test Loss: %.2f", loss)
+    print("Test Accuracy: %.2f", accuracy)
+
+#
+# Trains the MLP Regressor neural network.
+#
+def train_model_MLP():
     global grid_search
     # Read JSON data and convert into data frame.
     json = '<insert data>'
@@ -137,9 +185,23 @@ def train_model():
     print("R2 Score: %.2f" % r2_score(y_test, y_pred))
 
 #
-# Predict battery state of charge and send to dashboard.
+# Predict battery state of charge using LSTM and send to dashboard. (NOT YET TESTED)
 #
-def predict_soc(input):
+def predict_soc(voltage, current, temperature):
+    global normaliser, model, sio
+    data = np.array([[voltage, current, temperature]])
+    data = normaliser.transform(data)
+    prediction = model.predict(data)
+    json = {
+        "soc": 'test'
+    }
+    print("Predicted SOC: " + str(json))
+    sio.emit("send-twin-results", json)
+
+#
+# Predict battery state of charge using MLP Regressor and send to dashboard.
+#
+def predict_soc_MLP(input):
     global grid_search, sio
     prediction = grid_search.best_estimator_.predict(input)
     json = {
